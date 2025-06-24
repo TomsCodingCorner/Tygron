420836c231184e3e1250c2714563bf681d2da1e2
Tiescoolio <ties.smid@student.hu.nl>
2025-06-24 18:48:51 +0200

    refactor: rename files and centralize dataset paths
    Renamed several files to follow Git naming conventions for consistency.
    
    Added docstrings in accordance with project documentation standards.
    
    Created paths.py to define absolute paths to datasets.
    
    Updated all dependent .ipynb files to use paths.py for path resolution.
    
    Moved testInferenceWithIoU from ModelResearch/MaskRCNN/inference_training_research/py to Libraries/inference_training.py
    
    Removed Mask R-CNN library files from ModelResearch/MaskRCNN.

--- Libraries/coco_eval.py
+++ Libraries/coco_eval.py
@@ -5,7 +5,7 @@ from contextlib import redirect_stdout
 import numpy as np
 import pycocotools.mask as mask_util
 import torch
-import libraries.utils as utils
+import Libraries.utils as utils
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
